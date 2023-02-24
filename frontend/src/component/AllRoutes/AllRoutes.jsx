5baa22c50533e0776d98ed2f7f0f078b26dbd538
ujjawal0203 <ujjawal0203.jk@gmail.com>
2023-02-24 22:58:15 +0530

    up

--- frontend/src/component/AllRoutes/AllRoutes.jsx
+++ frontend/src/component/AllRoutes/AllRoutes.jsx
@@ -3,19 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import Admin from "../../Pages/Admin";
 import Kids from "../Category/Kids";
 import Mens from "../Category/Mens";
-import Women from "../Category/Womens";
-import HomePage from './../HomePage/HomePage';
+import Womens from "../Category/Womens";
 
+import HomePage from "./../HomePage/HomePage";
+import ProductDetails from "../Category/Productdetails";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/admin" element={<Admin />}></Route>
-      <Route path="/womens" element={<Women />} />
+      <Route path="/womens" element={<Womens />} />
       <Route path="/mens" element={<Mens />} />
       <Route path="/kids" element={<Kids />} />
-
+      <Route path="/product/:id" element={<ProductDetails />} />
     </Routes>
   );
 };
