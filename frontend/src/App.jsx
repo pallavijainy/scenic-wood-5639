10bd6e5ec48818904f45ba9eb745eb7ec933b298
ujjawal0203 <ujjawal0203.jk@gmail.com>
2023-02-24 13:32:25 +0530

    login & Signup day-4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 // import "./App.css";
 import { Box } from "@chakra-ui/react";
 import AllRoutes from "./component/AllRoutes/AllRoutes";
+import Footer from "./component/Footer/Footer"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
   return (
     <Box>
       <AllRoutes />
+      <Footer />
     </Box>
   );
 }
--- frontend/src/component/AllRoutes/AllRoutes.jsx
+++ frontend/src/component/AllRoutes/AllRoutes.jsx
@@ -5,17 +5,20 @@ import Kids from "../Category/Kids";
 import Mens from "../Category/Mens";
 import Women from "../Category/Womens";
 import HomePage from './../HomePage/HomePage';
+import Login from '../LoginSignupPage/Login';
+import Signup from '../LoginSignupPage/Signup'
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>}></Route>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
       <Route path="/admin" element={<Admin />}></Route>
-      <Route path="/womens" element={<Women/>} />
-    
-    <Route path="/mens" element={<Mens/>} />
-    <Route path="/kids" element={<Kids/>} />
-    
+      <Route path="/womens" element={<Women />} />
+      <Route path="/mens" element={<Mens />} />
+      <Route path="/kids" element={<Kids />} />
+
     </Routes>
   );
 };
