704b32c21fe7f47ee6d9ddbd22da092c11e87bee
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-02-24 20:03:54 +0530

    add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 // import "./App.css";
 import { Box } from "@chakra-ui/react";
 import AllRoutes from "./component/AllRoutes/AllRoutes";
+import Navbar from './component/Navbar/Navbar';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <Box>
+      <Navbar/>
       <AllRoutes />
     </Box>
   );
