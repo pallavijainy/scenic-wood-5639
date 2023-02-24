5c0946da456581b8f3f4c8e9f0fb22d3980a6439
rajato1209 <rajato1209@gmail.com>
2023-02-24 23:36:58 +0530

    cart page

--- frontend/src/component/AllRoutes/AllRoutes.jsx
+++ frontend/src/component/AllRoutes/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Cart from "../../Pages/Cart";
 import Admin from "../../Pages/Admin";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/admin" element={<Admin />}></Route>
+      <Route path="/cart" element={<Cart />}></Route>
     </Routes>
   );
 };
